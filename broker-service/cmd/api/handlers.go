6d6cc55d1f1ea0f0a4846bf8d21de7eda6695662
Denis Novikov <alpden550@gmail.com>
2023-07-08 17:47:25 +0300

    feat(handlers): add handling log action

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -14,11 +14,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(writer http.ResponseWriter, request *http.Request) {
@@ -45,6 +51,8 @@ func (app *Config) HandleSubmission(writer http.ResponseWriter, request *http.Re
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(writer, requestPayload.Auth)
+	case "log":
+		app.logItem(writer, requestPayload.Log)
 	default:
 		err := tool.WriteErrorJSON(writer, errors.New("unknown action"))
 		if err != nil {
@@ -94,6 +102,43 @@ func (app *Config) authenticate(writer http.ResponseWriter, a AuthPayload) {
 
 	err = tool.WriteJSON(writer, http.StatusAccepted, payload)
 	if err != nil {
+		return
+	}
+}
+
+func (app *Config) logItem(writer http.ResponseWriter, entry LogPayload) {
+	var tool helpers.Tool
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	loggerURL := fmt.Sprintf("%s/log", app.LoggerURL)
+	request, err := http.NewRequest("POST", loggerURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		_ = tool.WriteErrorJSON(writer, err)
+		return
+	}
+	log.Printf("%#v\n", entry)
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		_ = tool.WriteErrorJSON(writer, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		_ = tool.WriteErrorJSON(writer, err)
+		return
+	}
+
+	payload := helpers.JSONResponse{
+		Error:   false,
+		Message: "logged",
+	}
+	err = tool.WriteJSON(writer, http.StatusAccepted, payload)
+	if err != nil {
+		log.Printf("%e", fmt.Errorf("%e", err))
 		return
 	}
 }
