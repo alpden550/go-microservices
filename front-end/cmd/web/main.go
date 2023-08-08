934568b390458f8b31a17312095a1dc9b22386ab
Denis Novikov <alpden550@gmail.com>
2023-08-08 14:21:31 +0300

    refact(frontend): move handlers from main

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"embed"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 
@@ -35,42 +33,14 @@ func main() {
 		log.Fatalf("sentry.Init: %s", err)
 	}
 
-	td := templateData{BrokerURL: app.BrokerURL}
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		render(writer, "test.page.gohtml", &td)
-	})
-
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%s", app.Port),
+		Handler: app.routes(),
+	}
 	log.Printf("Starting front end service on port %s", app.Port)
-	err = http.ListenAndServe(fmt.Sprintf(":%s", app.Port), nil)
+
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
-	}
-}
-
-//go:embed templates
-var templateFS embed.FS
-
-func render(writer http.ResponseWriter, t string, td *templateData) {
-	partials := []string{
-		"templates/base.layout.gohtml",
-		"templates/header.partial.gohtml",
-		"templates/footer.partial.gohtml",
-	}
-
-	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
-
-	for _, x := range partials {
-		templateSlice = append(templateSlice, x)
-	}
-
-	tmpl, err := template.ParseFS(templateFS, templateSlice...)
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if err = tmpl.Execute(writer, td); err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
 	}
 }
